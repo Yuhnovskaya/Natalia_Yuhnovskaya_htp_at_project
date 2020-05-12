5aa3f9d0ba720e51033d1416ee164b7694d53b5b
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-05-12 13:43:00 +0300

    test

---
+++ src/test/java/webDriver/DriverManager.java
@@ -0,0 +1,10 @@
+package webDriver;
+
+import org.openqa.selenium.WebDriver;
+
+public class DriverManager {
+    private static WebDriver webDriver;
+    public  WebDriver getDriver() {
+        if (webDriver == null) ;
+    }
+}
