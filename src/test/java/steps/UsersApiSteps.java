73d0b248f8039b9238b06b5afeb4ce0aa6fb0d6b
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-05-23 22:44:39 +0300

    added

--- src/test/java/steps/UsersApiSteps.java
+++ src/test/java/steps/UsersApiSteps.java
@@ -1,4 +1,31 @@
-package Steps;
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
 
 public class UsersApiSteps {
+
+    @Given("I start execution")
+    public void iStartExecution() {
+        //throw new cucumber.api.PendingException();
+        System.out.println("given");
+    }
+
+    @When("I search user by {string} name")
+    public void iSearchUserByName(String string) {
+        System.out.println("when");
+        // throw new cucumber.api.PendingException();
+    }
+
+    @Then("I verify that I got {string}")
+    public void iVerifyThatIGot(String string) {
+        System.out.println("then");
+        // throw new cucumber.api.PendingException();
+    }
+
+
+
+
+
 }
