f02e3ac82780d4f13543e278b73b562b7839f70e
Yuhnovskaya <Yuhnovskaya@gmail.com>
2020-05-23 11:53:58 +0300

    added hometask14

---
+++ src/test/java/runners/JUnitRunner.java
@@ -0,0 +1,11 @@
+package runners;
+
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.searchTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({searchTest.class})
+public class JUnitRunner {
+}
